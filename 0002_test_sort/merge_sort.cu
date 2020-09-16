1644744a6239b61c239bf68da0ec8a2adfb4d58e
functionxu123 <1229853312@qq.com>
2020-09-16 20:47:39 +0800

    FIX: done merge sort

--- 0002_test_sort/merge_sort.cu
+++ 0002_test_sort/merge_sort.cu
@@ -55,7 +55,7 @@ __global__ void merge_sort(int *datas, int n){
 int main(){
     CUDA_CALL(cudaSetDevice(0));
     //init
-    const int length=1024;
+    const int length=1000;
     int a[length], b[length];
     for (int i=0;i<length;++i){
         a[i]=length-i;
@@ -68,7 +68,8 @@ int main(){
     //cudaError_t cudaMemcpy ( void* dst, const void* src, size_t count,cudaMemcpyKind kind )
     //cudaMemcpyHostToHost   cudaMemcpyHossToDevice   cudaMemcpyDeviceToHost   cudaMemcpuDeviceToDevice
     CUDA_CALL(cudaMemcpy(datas,a,length*sizeof(int),cudaMemcpyHostToDevice) );
-    merge_sort<<<1, length, length*2>>>(datas, length);
+    //注意这里传给kernel的shared mem大小是以字节度量的
+    merge_sort<<<1, length, length*2*sizeof(int)>>>(datas, length);
     CUDA_CALL(cudaGetLastError());
 
     CUDA_CALL( cudaThreadSynchronize());

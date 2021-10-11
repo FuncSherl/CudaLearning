62782bd15a83d58390b9b8e5a61f0f3ab08e6941
functionxu123 <1229853312@qq.com>
2021-10-11 21:51:41 +0800

    TODO : nms gpu;

---
+++ 0003_test_nms/nms_gpu.cu
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+
+const int imgsize=1024;
+const string sample_img="nms_cpu.jpg";
+const string boxfile="./boxes.txt";//x1, y1, x2, y2, score 
+const int eachboxlen=5;
+
+
+void split(const string & str, vector<string> &ret, const string delim=" "){
+	auto firpos=str.find_first_not_of(delim, 0);
+	auto secpos=str.find_first_of(delim, firpos);
+
+	while(firpos!=string::npos || secpos!=string::npos){
+		ret.push_back(str.substr(firpos, secpos-firpos));
+		firpos=str.find_first_not_of(delim, secpos);
+		secpos=str.find_first_of(delim, firpos);
+	}
+}
+
+template<class T>
+T StrtoNum(const string & s){
+	istringstream iss(s);  
+    T num;  
+    iss >> num;  
+    return num;      
+}
+
+//x1, y1, x2,y2,score
+__global__ void nms(float *boxes, int boxnum, float thresh){
+	
+}
+
+const vector<Scalar> COLORS={Scalar(255,0,0), Scalar(0,255,0), Scalar(0,0,255), Scalar(255,255,0), Scalar(0,255,255),Scalar(255,0,255),
+							 Scalar(125,255,0), Scalar(255,125,0), Scalar(0,125,255), Scalar(0,255,125), Scalar(255,0,125), Scalar(125,0,255)};
+
+
+int main(int argc, char *argv[]){
+	if(argc<2){
+		cout<<"Usage: command thresh"<<endl;
+		exit(1);
+	}
+	ifstream readf(boxfile);
+	string lin;
+	vector<vector<float>> boxes;//each row is : x1, y1, x2,y2,score
+	while(getline(readf, lin)){
+		vector<string> tep;
+		split(lin, tep, " ");
+		
+
+		vector<float> onebox;
+		for (auto &t: tep){
+			onebox.push_back(StrtoNum<float>(t));
+		}
+		
+		if (onebox.size()!=5) continue;//x1, y1, x2,y2,score
+
+		boxes.push_back(onebox);
+	}
+	cv::Mat ori(imgsize, imgsize, CV_8UC3, Scalar(cv::uint8_t(255),cv::uint8_t(255),cv::uint8_t(255)));
+	cv::Mat aft(imgsize, imgsize, CV_8UC3,Scalar(cv::uint8_t(255),cv::uint8_t(255),cv::uint8_t(255)));
+
+	auto fun_draw=[](const vector<vector<float>> & boxes, Mat & img){
+		int cnt=0;
+		for (auto & b: boxes){
+			int x1=b[0];
+			int y1=b[1];
+			int x2=b[2];
+			int y2=b[3];
+			float score=b[4];
+
+			rectangle(img, Point(x1, y1), Point(x2, y2), COLORS[cnt%COLORS.size()]);
+			putText(img, to_string(score), Point(x1, y1-4), cv::FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0,0,255));
+			++cnt;
+		}
+	};
+	fun_draw(boxes, ori);
+	imwrite("ori_"+sample_img, ori);
+
+	cout<<"boxes before: "<<boxes.size()<<endl;
+	nms(boxes, StrtoNum<float>(string(argv[1])));
+	cout<<"boxes after: "<<boxes.size()<<endl;
+
+	fun_draw(boxes, aft);
+	imwrite("aft_"+sample_img, aft);
+
+	return 0;
+}
